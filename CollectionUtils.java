8b24cd533b3609eb02182a1a65a4a7429b0ab09b
unknown <tprashan@INTPRASHAN.corporate.thoughtworks.com>
2015-02-28 16:28:01 +0530

    filter is done with generic type but not tested.

--- CollectionUtils.java
+++ CollectionUtils.java
@@ -4,13 +4,26 @@ interface ListMapper<E>{
 	public E callback(E ele);
 }
 
+interface ListFilter<E>{
+	public Boolean filterCallback(E ele);
+}
+
 public class CollectionUtils{
 	
-	public static<E> List<E> map(List<E> list, ListMapper fun){
+	public static<E> List<E> map(List<E> list, ListMapper method){
 		List<E> myList = new ArrayList<E>();
 		for (E i: list) {
-			myList.add((E)fun.callback(i));
+			myList.add((E)method.callback(i));
 		}
 		return myList;
 	}
+
+	public static<E> List<E> filter(List<E> list, ListFilter method){	
+		List<E> myList = new ArrayList<E>();
+		for (E i: list) {
+			if((boolean)method.filterCallback(i))
+				myList.add(i);
+		}
+		return myList;
+	} 
 }
--- VersionTest.java
+++ VersionTest.java
@@ -21,6 +21,24 @@ class convertString implements ListMapper<String>{
 	}
 }
 
+class returnGreaterThanFive implements ListFilter<Integer>{
+	public Boolean filterCallback(Integer element){
+		return element>5;
+	}
+}
+
+class returnEvenNumber implements ListFilter<Integer>{
+	public Boolean filterCallback(Integer element){
+		return (element%2==0)?true:false;
+	}
+}
+
+class returnOddNumber implements ListFilter<Integer>{
+	public Boolean filterCallback(Integer element){
+		return (element%2!=0)?true:false;
+	}
+}
+
 public class VersionTest{
 	private static final double DELTA = 1e-15;
 
@@ -47,5 +65,29 @@ public class VersionTest{
 		list.add("Dolly");
 		List<String> newlist = CollectionUtils.map(list,new convertString());
 		assertEquals((String)newlist.get(0),"Hello");
+	}
+	@Test
+	public void return_number_grreater_than_five(){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(4);
+		list.add(7);
+		List<Integer> newlist = CollectionUtils.filter(list,new returnGreaterThanFive());
+		assertEquals((int)newlist.get(0),7);
+	}
+	@Test
+	public void return_list_of_even_numbers(){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(4);
+		list.add(7);
+		List<Integer> newlist = CollectionUtils.filter(list,new returnEvenNumber());
+		assertEquals((int)newlist.get(0),4);
+	}
+	@Test
+	public void return_list_of_odd_numbers(){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(4);
+		list.add(7);
+		List<Integer> newlist = CollectionUtils.filter(list,new returnOddNumber());
+		assertEquals((int)newlist.get(0),7);
 	}
 }
