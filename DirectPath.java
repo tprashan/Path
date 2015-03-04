6b7ba9f32dff83110e80b7686761f0a86de4b889
unknown <tprashan@INTPRASHAN.corporate.thoughtworks.com>
2015-03-04 13:42:27 +0530

    find path method is modified.

--- DirectPath.java
+++ DirectPath.java
@@ -3,12 +3,13 @@ import java.util.*;
 class Database{
 	public static Map<String,List<String>> createDataBase(){
 		Map<String,List<String>> map = new HashMap<String,List<String>>();
-		map.put("Bangalore", new ArrayList(Arrays.asList("Singapore")));
-		map.put("Singapore", new ArrayList(Arrays.asList("Seoul", "Bangalore","Dubai")));
-		map.put("Seoul", new ArrayList(Arrays.asList("Beijing", "Singapore")));
-		map.put("Beijing", new ArrayList(Arrays.asList("Tokyo", "Seoul")));
-		map.put("Tokyo", new ArrayList(Arrays.asList("Beijing")));
-		map.put("Dubai", new ArrayList(Arrays.asList("Lucknow")));
+		map.put("Bangalore", new ArrayList<String>(Arrays.asList("Singapore")));
+		map.put("Singapore", new ArrayList<String>(Arrays.asList("Seoul", "Bangalore","Dubai")));
+		map.put("Seoul", new ArrayList<String>(Arrays.asList("Beijing", "Singapore")));
+		map.put("Beijing", new ArrayList<String>(Arrays.asList("Tokyo", "Seoul")));
+		map.put("Tokyo", new ArrayList<String>(Arrays.asList("Beijing")));
+		map.put("Dubai", new ArrayList<String>(Arrays.asList("Lucknow")));
+		map.put("Delhi", new ArrayList<String>(Arrays.asList("Kanpur")));
 		return map;
 	}
 }
@@ -27,9 +28,9 @@ class CheckDatabase{
 
 class DirectPath{
 	public static Queue<String> queue = new LinkedList<String>();
+	public static Map<String,List<String>> db =  Database.createDataBase();
 
 	public static Boolean findDirectPath(String src,String des){
-		Map<String,List<String>> db =  Database.createDataBase();
 		for(String city: db.keySet()) {
 			if(city.equals(src) && db.get(city).contains(des)){
 				queue.add(src);queue.add(des);
@@ -39,22 +40,25 @@ class DirectPath{
 		return false;
 	}
 
-	public static Boolean findPath(String src,String des){
-		Map<String,List<String>> db =  Database.createDataBase();
-		if(src.equals(des)) return true;
-		queue.add(src);
-		if((db.keySet().contains(src))){
-			for(String city: db.get(src)) {
-				if((db.keySet().contains(city)) && db.get(city).contains(des)){
-					queue.add(city);queue.add(des);
-					return true;
-				}
-			}
-			for(String city: db.get(src)) {
-				if(!queue.contains(city))
-					return findPath(city,des);
+	public static Boolean findDestination(String src,String des){
+		for(String city: db.get(src)) {
+			if((db.keySet().contains(city)) && db.get(city).contains(des)){
+				queue.add(city);queue.add(des);
+				return true;
 			}
 		}
+		for(String city: db.get(src)) {
+			if(!queue.contains(city))
+				return findPath(city,des);
+		}
+		return false;
+	}
+
+	public static Boolean findPath(String src,String des){
+		if(src.equals(des)) return true;
+		queue.add(src);
+		if((db.keySet().contains(src)))
+			return findDestination(src,des);
 		return false;
 	}
 }
