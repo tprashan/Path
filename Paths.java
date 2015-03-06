f42344ef1fd4b2be4bcd62ec8797e6ffd92188f0
unknown <tprashan@INTPRASHAN.corporate.thoughtworks.com>
2015-03-06 20:09:59 +0530

    git name is modified and version 3 is working with reverse order path.

--- Paths.java
+++ Paths.java
@@ -14,12 +14,12 @@ class PrintPath{
 public class Paths{
 	public static Boolean findPaths(String src,String des)throws Exception{
 	    Boolean directPath = DirectPath.findDirectPath(src,des);
-		if(directPath)
-			throw new Exception("Full Path is: "+PrintPath.printPath(src,des));//return true;
-		if(!SearchInDatabase.isCityPresentInDatabase(src))
+			if(!SearchInDatabase.isCityPresentInDatabase(src))
 			throw new Exception("No city Named : "+ src +" in database.");
 		if(!SearchInDatabase.isCityPresentInDatabase(des))
 			throw new Exception("No city Named : "+des +" in database.");
+		if(directPath)
+			throw new Exception("Full Path is: "+PrintPath.printPath(src,des));//return true;
 	    Boolean fullPath = DirectPath.findPath(src,des);
 	    if(fullPath)
 	    	throw new Exception("Full Path is: "+PrintPath.printPath(src,des)); 
